23f7310e3b24bb54a8e115750efcfc010767eb6c
BeOrNot2Be <timchick.ua@gmail.com>
2020-03-26 15:10:13 -0700

    testing in development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,7 +30,6 @@ import { ProfileProjectsPageComponent } from './components/profile-projects-page
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule,
     HttpClientModule,
     BrowserAnimationsModule,
     LyThemeModule.setTheme('minima-light'),
